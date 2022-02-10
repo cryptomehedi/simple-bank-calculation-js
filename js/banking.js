a01a45262793fe28e994a5a195028e76303937a0
cryptomehedi <classicalmehedi@gmail.com>
2022-02-10 17:02:25 +0600

    warning add

--- js/banking.js
+++ js/banking.js
@@ -25,21 +25,52 @@ document.getElementById('depositBtn').addEventListener('click', function(){
 
 document.getElementById('withdrawBtn').addEventListener('click', function(){
     // get the amount 
+    
+    
     const withdrawInput = document.getElementById('withdraw-input');
     const newWithdrawAmountText = withdrawInput.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountText);
-    // update withdraw total 
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const previousWithdrawAmountText = withdrawTotal.innerText;
-    const previousWithdrawAmount = parseFloat(previousWithdrawAmountText);
-    const newWithdrawTotal = previousWithdrawAmount + newWithdrawAmount ;
-    withdrawTotal.innerText = newWithdrawTotal;
-// update balance 
+    
+    // update balance 
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-    balanceTotal.innerText = newBalanceTotal;
-    // clear input 
-    withdrawInput.value = '';
+
+
+    if(0 > newBalanceTotal){
+        const ClickText = document.getElementById('warningText');
+        ClickText.innerText = 'insufficient balance'
+        withdrawInput.value = '';
+    }else{
+        // update withdraw total 
+        const withdrawTotal = document.getElementById('withdraw-total');
+        const previousWithdrawAmountText = withdrawTotal.innerText;
+        const previousWithdrawAmount = parseFloat(previousWithdrawAmountText);
+        const newWithdrawTotal = previousWithdrawAmount + newWithdrawAmount ;
+        withdrawTotal.innerText = newWithdrawTotal;
+        // update balance 
+        balanceTotal.innerText = newBalanceTotal;
+        withdrawInput.value = '';
+    }
+    
 });
+
+
+
+
+// const withdrawInput = document.getElementById('withdraw-input');
+//     const newWithdrawAmountText = withdrawInput.value;
+//     const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+//     // update withdraw total 
+//     const withdrawTotal = document.getElementById('withdraw-total');
+//     const previousWithdrawAmountText = withdrawTotal.innerText;
+//     const previousWithdrawAmount = parseFloat(previousWithdrawAmountText);
+//     const newWithdrawTotal = previousWithdrawAmount + newWithdrawAmount ;
+//     withdrawTotal.innerText = newWithdrawTotal;
+//     // update balance 
+//     const balanceTotal = document.getElementById('balance-total');
+//     const balanceTotalText = balanceTotal.innerText;
+//     const previousBalanceTotal = parseFloat(balanceTotalText);
+//     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+//     balanceTotal.innerText = newBalanceTotal;
