bfaa80bd7b1851862a2c028dc52daba611721c5e
cryptomehedi <classicalmehedi@gmail.com>
2022-02-12 02:28:02 +0600

    copy btn add

--- js/bank.js
+++ js/bank.js
@@ -5,7 +5,7 @@ document.getElementById('login-submit').addEventListener('click', function(){
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
 
-    if( userEmail == 'test@gmail.com' && userPassword == 'test'){
+    if( userEmail == 'test@gmail.com' && userPassword == 'test' || userEmail == 'test@test.com' && userPassword == 'test' || userEmail == 'admin@admin.com' && userPassword == 'admin' || userEmail == 'admin@gmail.com' && userPassword == 'admin'){
         window.location.href = 'banking.html'
     }else{
         const warningEmail = document.getElementById('warning-email');
@@ -13,3 +13,12 @@ document.getElementById('login-submit').addEventListener('click', function(){
         withdrawInput.value = '';
     }
 });
+
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
